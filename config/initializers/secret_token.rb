49e04c6406ec8767415723056cc97b111b445d20
Alain Dejoux <adejoux@krystalia.net>
2014-02-20 06:39:57 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniInventory::Application.config.secret_key_base = '6a2102aac402a79992cdb689c13aed2946194a8305c7b12c3725daab9a62cc8296bba9eecdb7c55110d2c3f5fe6cde60f621b2563155c4cbcf628ddd463f57d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmniInventory::Application.config.session_store :cookie_store, key: '_omni_inventory_session'
